9b8a39976c41f787f0ca8dae1f176fe24160fae8
Corey Gibson <cgibsonmm@gmail.com>
2017-09-19 16:24:23 -0400

    changes file to check changes

--- bot.rb
+++ bot.rb
@@ -1,57 +1,101 @@
 require 'yaml'
 require_relative 'wordplay'
 
+# A basic implementation of a chatterbot
 class Bot
   attr_reader :name
 
+  # Initializes the bot object, loads in the external YAML data
+  # file and sets the bot's name. Raises an exception if
+  # the data loading process fails.
   def initialize(options)
-    @name = options[:name] || 'Unnamed Bot'
+    @name = options[:name] || "Unnamed Bot"
     begin
-      @data = YAML.load(File.read(options[:data_file]))
+      @data = YAML.load(File.open(options[:data_file]).read)
     rescue
       raise "Can't load bot data"
     end
   end
 
-
+  # Returns a random greeting as specified in the bot's data file
   def greeting
-    random_response :greeting
+    random_response(:greeting)
   end
 
-  def farwell
-    random_response :farwell
+  # Returns a random farewell message as specified in the bot's
+  # data file
+  def farewell
+    random_response(:farewell)
   end
 
+  # Responds to input text as given by a user
   def response_to(input)
     prepared_input = preprocess(input.downcase)
     sentence = best_sentence(prepared_input)
+    reversed_sentence = WordPlay.switch_pronouns(sentence)
     responses = possible_responses(sentence)
     responses[rand(responses.length)]
   end
 
+
   private
 
+  # Chooses a random response phrase from the :responses hash
+  # and substitutes metadata into the phrase
+  def random_response(key)
+    random_index = rand(@data[:responses][key].length)
+    @data[:responses][key][random_index].gsub(/\[name\]/, @name)
+  end
+
+  # Performs preprocessing tasks upon all input to the bot
+  def preprocess(input)
+    perform_substitutions(input)
+  end
+
+  # Substitutes words and phrases on supplied input as dictated by
+  # the bot's :presubs data
+  def perform_substitutions(input)
+    @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
+    input
+  end
+
+  # Using the single word keys from :responses, we search for the
+  # sentence that uses the most of them, as it's likely to be the
+  # 'best' sentence to parse
+  def best_sentence(input)
+    hot_words = @data[:responses].keys.select do |k|
+      k.class == String && k =~ /^\w+$/
+    end
+
+    WordPlay.best_sentence(input.sentences, hot_words)
+  end
+
+
+
+  # Using a supplied sentence, go through the bot's :responses
+  # data set and collect together all phrases that could be
+  # used as responses
   def possible_responses(sentence)
     responses = []
 
-    # find all patterns to try to match aganist
-    @date[:responses].keys.each do |pattern|
+    # Find all patterns to try to match against
+    @data[:responses].keys.each do |pattern|
       next unless pattern.is_a?(String)
 
-      # For each pattern, see if the supplied sentences
-      # contains a match. Remove susbstitution symbols (*)
-      # before checking. Push all responses to array.
+      # For each pattern, see if the supplied sentence contains
+      # a match. Remove substitution symbols (*) before checking.
+      # Push all responses to the responses array.
       if sentence.match('\b' + pattern.gsub(/\*/, '') + '\b')
-        # if the pattern contains substitution placeholders,
-        # pefrorm the susbstitutions
+        # If the pattern contains substitution placeholders,
+        # perform the substitutions
         if pattern.include?('*')
           responses << @data[:responses][pattern].collect do |phrase|
-            # first erase everything before the placeholder,
-            # leaving everything after it.
-            matching_section = sentence.sub(/\^.*#{pattern}\s+/, '')
+            # First, erase everything before the placeholder
+            # leaving everything after it
+            matching_section = sentence.sub(/^.*#{pattern}\s+/, '')
 
-            # Then substitute the text after the placeholder, with the
-            # pronouns switched
+            # Then substitute the text after the placeholder, with
+            # the pronouns switched
             phrase.sub('*', WordPlay.switch_pronouns(matching_section))
           end
         else
@@ -61,32 +105,11 @@ class Bot
       end
     end
 
-    # if there were no matches, add the default ones
+    # If there were no matches, add the default ones
     responses << @data[:responses][:default] if responses.empty?
 
-    # flatten the blockes of responses to a flat array
+    # Flatten the blocks of responses to a flat array
     responses.flatten
   end
 
-  def best_sentence(input)
-    hot_words = @data[:responses].keys.select do |k|
-      k.class == String && k =~ /^\w+$/
-    end
-
-    WordPlay.best_sentence(input.sentences, hot_words)
-  end
-
-  def preprocess(input)
-    perform_substitutions(input)
-  end
-
-  def perform_substitutions(input)
-    @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
-    input
-  end
-
-  def random_response(key)
-    random_index = rand(@data[:responses][key].length)
-    @data[:responses][key][random_index].gsub(/\[name\]/, @name)
-  end
 end
